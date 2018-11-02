b9365924e637352e3408194acea75b5f13be78e8
Jamie Cho <jchocholate@gmail.com>
2018-11-02 00:21:29 -0400

    fix odom conversion logic

--- robot_learning/scripts/convert_odom.py
+++ robot_learning/scripts/convert_odom.py
@@ -8,7 +8,7 @@ from tf_conversions import posemath as pm
 class ConvertOdom(object):
     def __init__(self):
         self.method_ = rospy.get_param('~method', default='tf')
-        self.pub_tf_ = rospy.get_param('~pub_tf', default=True)
+        self.pub_tf_ = rospy.get_param('~pub_tf', default=False)
 
         # data
         self.odom_ = None
@@ -19,7 +19,7 @@ class ConvertOdom(object):
         # create ROS handles
         self.tfl_ = tf.TransformListener()
         self.tfb_ = tf.TransformBroadcaster()
-        self.sub_ = rospy.Subscriber('odom', Odometry, self.odom_cb)
+        self.sub_ = rospy.Subscriber('/android/odom', Odometry, self.odom_cb)
         self.pub_ = rospy.Publisher('cvt_odom', Odometry, queue_size=10)
 
         self.init_tf()
@@ -50,19 +50,22 @@ class ConvertOdom(object):
             T0 = pm.toMatrix(pm.fromMsg(pose)) # android_odom --> android
 
             # send odom -> base_link transform
-            T = tf.transformations.concatenate_matrices([self.T_,T0,self.Ti_])
+            T = tf.transformations.concatenate_matrices(self.T_,T0,self.Ti_)
             frame = pm.fromMatrix(T)
             if self.pub_tf_:
                 txn, qxn = pm.toTf(frame)
-                self.tfb_.sendTransform(txn, qxn, self.odom_.header.stamp)
+                self.tfb_.sendTransform(txn, qxn, self.odom_.header.stamp,
+                        'odom',
+                        'base_link'
+                        )
 
             # send as msg
             # TODO : does not deal with twist/covariance information
             msg = pm.toMsg(frame)
             self.cvt_msg_.pose.pose = pm.toMsg(frame)
-            self.cvt_msg_.header.frame_id = 'odom'
+            self.cvt_msg_.header.frame_id = 'map' # experimental
             self.cvt_msg_.header.stamp = self.odom_.header.stamp
-            self.pub_.publish(msg)
+            self.pub_.publish(self.cvt_msg_)
 
     def run(self):
         rate = rospy.Rate(50)
