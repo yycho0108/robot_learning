7583c3154a8d3af898c0359fa000a6ff18c8860e
yycho0108 <jchocholate@gmail.com>
2018-11-19 09:30:13 -0500

    apparently working-ish

--- robot_learning/scripts/eval_vo.py
+++ robot_learning/scripts/eval_vo.py
@@ -19,7 +19,7 @@ def main():
     # restore/train flags
     # checkpoint file to restore from
     # restore_ckpt = '/tmp/vo/20/ckpt/model.ckpt-4'
-    restore_ckpt = '/tmp/vo/30/ckpt/model.ckpt-1200'
+    restore_ckpt = '/tmp/vo/37/ckpt/model.ckpt-2000'
     is_training = False
 
     # override cfg params
